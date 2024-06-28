8cd60e9d321ad3ab197c9a329dc8c828b9456182
fateme vaziri <155662845+ftvaziri@users.noreply.github.com>
2024-06-28 13:05:33 +0330

    [enhancement] login and signup buttons

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,16 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Main implements ActionListener {
+
     public static JFrame frame;
-    public loginPanel myLoginPanel;
-    public signupPanel mySignupPanel;
+
+    // OBJECTS:
+    public loginPanel loginPanel;
+    public signupPanel signupPanel;
+    public userProfilePanel userProfilePanel;
+    public sellerProfilePanel sellerProfilePanel;
+
+    // DESIGN:
     public  Color primaryColor = Color.decode("#FF841F"), secondaryColor = Color.gray;
     public  Font font = new Font("Geeza Pro",  Font.PLAIN, 15);
     public Main(){
@@ -14,9 +21,22 @@ public class Main implements ActionListener {
         frame.setSize(1080, 720);
         frame.setLayout(null);
 
-        myLoginPanel = new loginPanel(frame);
-        mySignupPanel = new signupPanel(frame);
-        myLoginPanel.signupButton.getButton().addActionListener(this);
+        // ----- login panel -----
+        loginPanel = new loginPanel(frame);
+        loginPanel.loginButton.getButton().addActionListener(this);
+        loginPanel.signupButton.getButton().addActionListener(this);
+
+        // ----- signup panel -----
+        signupPanel = new signupPanel(frame);
+        signupPanel.loginButton.getButton().addActionListener(this);
+        signupPanel.signupButton.getButton().addActionListener(this);
+
+        // ----- user profile panel -----
+        userProfilePanel = new userProfilePanel(frame);
+
+        // ----- seller profile panel -----
+        sellerProfilePanel = new sellerProfilePanel(frame);
+
 
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -32,23 +52,23 @@ public class Main implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 
-        if (e.getSource() == myLoginPanel.loginButton.getButton()){
+        if (e.getSource() == loginPanel.loginButton.getButton()){
             // todo (KIANA):
             //  1. validation
             //  2. if userType == user
-//            loginPanel.loginButton.goTo(loginPanel.allPanel, userProfilePanel.allPanel, frame);
+            loginPanel.loginButton.goTo(loginPanel.allPanel, userProfilePanel.allPanel, frame);
             // todo (KIANA):
             //  3. if userType == seller
-//            loginPanel.loginButton.goTo(loginPanel.allPanel, sellerProfilePanel.allPanel, frame);
-        } else if (e.getSource() == myLoginPanel.signupButton.getButton()) {
-            myLoginPanel.signupButton.goTo(myLoginPanel.allPanel, mySignupPanel.allPanel, frame);
+            loginPanel.loginButton.goTo(loginPanel.allPanel, sellerProfilePanel.allPanel, frame);
+        } else if (e.getSource() == loginPanel.signupButton.getButton()) {
+            loginPanel.signupButton.goTo(loginPanel.allPanel, signupPanel.allPanel, frame);
         }
-//        else if (e.getSource() == signupPanel.signupButton.getButton()) {
-//            // todo (KIANA):
-//            //  1. validation
-//        } else if (e.getSource() == signupPanel.loginButton.getButton()) {
-//            signupPanel.loginButton.goTo(signupPanel.allPanel, loginPanel.allPanel, frame);
-//        }
+        else if (e.getSource() == signupPanel.signupButton.getButton()) {
+            // todo (KIANA):
+            //  1. validation
+        } else if (e.getSource() == signupPanel.loginButton.getButton()) {
+            signupPanel.loginButton.goTo(signupPanel.allPanel, loginPanel.allPanel, frame);
+        }
     }
 
     // todo (FATEME):
