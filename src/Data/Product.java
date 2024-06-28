4e62348c9f13af87f8a0f66ee11aad53f80d7349
fateme vaziri <155662845+ftvaziri@users.noreply.github.com>
2024-06-29 02:11:22 +0330

    [enhancement] add image icon

--- src/Data/Product.java
+++ src/Data/Product.java
@@ -13,12 +13,16 @@ public class Product extends Component {
     private int score;
     private int purchaseFrequency;
     private Seller seller;
+<<<<<<< Updated upstream
     JLabel pictureLabel = new JLabel();
     String filePath = "\\Users\\kiana\\OneDrive\\Desktop\\Photos";
     File file;
     JFileChooser fileChooser = new JFileChooser(filePath);
 
     static ArrayList<Product> products = new ArrayList<>();
+=======
+    public static ArrayList<Product> products = new ArrayList<>();
+>>>>>>> Stashed changes
     Product(String name, int code, int price, int stock, Seller seller) {
         this.name = name;
         this.code = code;
