94c56119ee5983f12d93d61102abdb329afd65af
fateme vaziri <155662845+ftvaziri@users.noreply.github.com>
2024-06-29 19:20:09 +0330

    [enhancement] customersListPanel (GUI)

--- src/listPanels/customersListPanel.java
+++ src/listPanels/customersListPanel.java
@@ -16,11 +16,11 @@ public class customersListPanel extends themeTwoPanel {
 
         // text label (edit)
         titleLabel.setText("لیست کاربران");
-        titleLabel.setBounds(465, 40, 150, 30);
+        titleLabel.setBounds(390, 70, 300, 60);
 
         // back base.button
         backButton = new button("برگشت", Color.white, allPanel);
-        backButton.getButton().setBounds(150, 40, 100, 30);
+        backButton.getButton().setBounds(200, 80, 100, 30);
 
     }
 }
