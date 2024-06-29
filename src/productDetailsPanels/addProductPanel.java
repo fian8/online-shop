c521e34ff02fa06a5c6c48733186e0b87d34586b
fateme vaziri <155662845+ftvaziri@users.noreply.github.com>
2024-06-29 09:39:59 +0330

    [debug] null product

--- src/productDetailsPanels/addProductPanel.java
+++ src/productDetailsPanels/addProductPanel.java
@@ -25,34 +25,5 @@ public class addProductPanel extends productDetailsPanel {
         addTheProductButton = new button("افزودن محصول", Color.white, mainPanel);
         addTheProductButton.getButton().setBounds(300, 390, 150, 30);
 
-        // create product
-        Validation validator = new Validation();
-        String name = nameField.getText();
-        String price = priceField.getText();
-        String stock = stockField.getText();
-        int priceInt = 0, stockInt = 0;
-
-        int i;
-        for (i = 0; i < price.length(); i++) {
-            if (price.charAt(i) < '0' || price.charAt(i) > '9') {
-                JOptionPane.showMessageDialog(null, "مبلغ وارد شده معتبر نیست!");
-                break;
-            } else {
-                priceInt += price.charAt(i);
-            }
-        }
-        int j;
-        for (j = 0; j < stock.length(); j++) {
-            if (stock.charAt(j) < '0' || stock.charAt(j) > '9') {
-                JOptionPane.showMessageDialog(null, "عدد وارد شده معتبر نیست!");
-                break;
-            } else {
-                stockInt += stock.charAt(j);
-            }
-        }
-        if (i == price.length() && j == stock.length() && validator.nameValidation(name)) {
-            product = new Product(name, priceInt, stockInt);
-        }
-
     }
 }
