9f36b6973a56f5821cc7b477c8b6dd4c1cd9d53b
kianarzv <kiana.razavi1383@gmail.com>
2024-06-29 19:02:09 +0330

    [Debug]
    change product cart

--- src/productsPanels/productCard.java
+++ src/productsPanels/productCard.java
@@ -10,18 +10,18 @@ public class productCard {
     public JPanel cardPanel;
     public productCard(Product product) {
 
+        cardPanel.setLayout(null);
+        cardPanel.setPreferredSize(new Dimension(300, 220));
         nameLabel = new JLabel(product.getName());
-//        nameLabel.setBounds();
+        nameLabel.setBounds(25, 140, 275, 40);
         priceLabel = new JLabel(String.valueOf(product.getPrice()));
-//        priceLabel.setBounds();
+        priceLabel.setBounds(25, 180, 275, 40);
         imageLabel = new JLabel(product.imageIcon);
-//        imageLabel.setBounds();
-
+        imageLabel.setBounds(25, 25, 275, 140);
         cardPanel.add(imageLabel);
         cardPanel.add(nameLabel);
         cardPanel.add(priceLabel);
 
-        cardPanel.setPreferredSize(new Dimension(300, 220));
 
     }
 }
